f8b4138e81ae037aea1e41a1bd8875cea77aa3f7
Craig Buckler <craig@optimalworks.net>
2019-01-08 12:27:18 +0000

    switched to minimalanalytics

--- src/js/main/analytics.js
+++ src/js/main/analytics.js
@@ -3,50 +3,32 @@
 
   // requires: lib.js
 */
-/* global ow gtag dataLayer */
-(function() {
+/* global ow ma */
+setTimeout(function() {
 
   'use strict';
 
-  if (!ow.analytics || ow.devBuild || location.host.indexOf('.net') < 0 || !document.head) return;
+  if (!ow.analytics || ow.devBuild || location.host.indexOf('.net') < 0) return;
 
-  // load analytics API
-  setTimeout(function() {
+  // minimal analytics from https://minimalanalytics.com/
+  (function(a,b,c){var d=a.history,e=document,f=navigator||{},g=localStorage,h=encodeURIComponent,i=d.pushState,k=function(){return Math.random().toString(36)},l=function(){return g.cid||(g.cid=k()),g.cid},m=function(r){var s=[];for(var t in r)r.hasOwnProperty(t)&&void 0!==r[t]&&s.push(h(t)+'='+h(r[t]));return s.join('&')},n=function(r,s,t,u,v,w,x){var z='https://www.google-analytics.com/collect',A=m({v:'1',ds:'web',aip:c.anonymizeIp?1:void 0,tid:b,cid:l(),t:r||'pageview',sd:c.colorDepth&&screen.colorDepth?screen.colorDepth+'-bits':void 0,dr:e.referrer||void 0,dt:e.title,dl:e.location.origin+e.location.pathname+e.location.search,ul:c.language?(f.language||'').toLowerCase():void 0,de:c.characterSet?e.characterSet:void 0,sr:c.screenSize?(a.screen||{}).width+'x'+(a.screen||{}).height:void 0,vp:c.screenSize&&a.visualViewport?(a.visualViewport||{}).width+'x'+(a.visualViewport||{}).height:void 0,ec:s||void 0,ea:t||void 0,el:u||void 0,ev:v||void 0,exd:w||void 0,exf:'undefined'!=typeof x&&!1==!!x?0:void 0});if(f.sendBeacon)f.sendBeacon(z,A);else{var y=new XMLHttpRequest;y.open('POST',z,!0),y.send(A)}};d.pushState=function(r){return'function'==typeof d.onpushstate&&d.onpushstate({state:r}),setTimeout(n,c.delay||10),i.apply(d,arguments)},n(),a.ma={trackEvent:function o(r,s,t,u){return n('event',r,s,t,u)},trackException:function q(r,s){return n('exception',null,null,null,null,r,s)}}})(window,ow.analytics,{anonymizeIp:true,colorDepth:true,characterSet:true,screenSize:true,language:true});
 
-    var gScript = document.createElement('script');
-    gScript.onload = initAnalytics;
-    gScript.async = 1;
-    document.head.appendChild(gScript);
-    gScript.src = 'https://www.googletagmanager.com/gtag/js?id=' + ow.analytics;
+  if (!ma || !ma.trackEvent || !window.addEventListener) return;
 
-  }, 200);
+  // telephone/mail custom event
+  document.body.addEventListener('click', function(e) {
 
+    if (!e || !e.target || !e.target.href) return;
 
-  // initialise Analytics
-  function initAnalytics() {
+    var type = String(e.target.href).trim().toLowerCase().match(/^[^:]+:/);
+    if (!type || !type.length) return;
+    type = type[0];
 
-    // record page view
-    window.dataLayer = window.dataLayer || [];
-    window.gtag = function() { dataLayer.push(arguments); };
-    gtag('js', new Date());
-    gtag('config', ow.analytics);
+    // record event
+    if (type === 'tel:' || type === 'mailto:') {
+      ma.trackEvent('contact', type.slice(0, -1));
+    }
 
-    // telephone/mail custom event
-    if (window.addEventListener) document.body.addEventListener('click', function(e) {
+  }, false);
 
-      if (typeof gtag === 'undefined' || !e || !e.target || !e.target.href) return;
-
-      var type = String(e.target.href).trim().toLowerCase().match(/^[^:]+:/);
-      if (!type || !type.length) return;
-      type = type[0];
-
-      // record event
-      if (type === 'tel:' || type === 'mailto:') {
-        gtag('event', 'contact', { 'method': type.slice(0, -1) });
-      }
-
-    }, false);
-
-  }
-
-})();
+}, 300);
